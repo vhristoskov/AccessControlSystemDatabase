256ba9a806b6d7c63b9d02da0a72d2580daa074d
Victor Hristoskov <victor_hristoskov@mail.bg>
2012-06-11 02:42:49 +0300

    10 rows with data added to Department table

---
+++ TableDataInsertion.sql
@@ -0,0 +1,12 @@
+insert into Department
+values 
+('iOS Developing'),
+('Quality Assurance'),
+('Finance'),
+('.NET Developing'),
+('Sales'),
+('Marketing'),
+('Document Control'),
+('Information Services'),
+('Java Developing'),
+('Android Developing')

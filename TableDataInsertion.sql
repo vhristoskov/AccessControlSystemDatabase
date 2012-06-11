69471ed639c3af28125b879ad544e5993df6b9f4
Victor Hristoskov <victor_hristoskov@mail.bg>
2012-06-11 03:14:10 +0300

    Added info about 10 places and 10 addresses using cursor statement like a for loop and created stored procedure addPlaceToDepartment

--- TableDataInsertion.sql
+++ TableDataInsertion.sql
@@ -10,3 +10,43 @@ values
 ('Information Services'),
 ('Java Developing'),
 ('Android Developing')
+
+-- =============================================
+-- Fill In @placeName  = Entrance Door 
+-- for every one of departments
+-- with help of CURSOR statement
+-- =============================================
+
+DECLARE @departmName varchar(50),
+		@floorNum int
+
+--Creating the cursor 
+DECLARE Departments CURSOR FOR
+SELECT name from Department
+
+SET @floorNum = 1
+
+OPEN Departments 
+FETCH Departments INTO @departmName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	exec [dbo].addPlaceToDepartment
+		@departmentName = @departmName,
+		@placeCity = 'Sofia',
+		@placeName = 'Entrance Door',
+		@placeStreet = 'Malinova Dolina, 12',
+		@placeBuildingName = 'Mall Malinka',
+		@placeBuldingFloor = @floorNum
+	
+	SET @floorNum = @floorNum + 1
+
+	FETCH Departments INTO @departmName
+END
+
+CLOSE Departments
+DEALLOCATE Departments
+
+
+select * from Address
+
